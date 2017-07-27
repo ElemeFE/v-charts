ba606cd7b7573c52f1e9cef9d58314a2948631b2
quietcoder <1642965215@qq.com>
2017-07-27 17:17:09 +0800

    新增：饼图增加属性 limitShowNum, roseType, level

--- src/pie/pie.js
+++ src/pie/pie.js
@@ -4,6 +4,7 @@ import 'echarts/lib/chart/pie'
 
 const pieRadius = 100
 const ringRadius = [80, 100]
+const roseRingRadius = [20, 100]
 const pieOffsetY = 200
 
 function getPieSeries (args) {
@@ -17,55 +18,147 @@ function getPieSeries (args) {
     offsetY,
     selectedMode,
     hoverAnimation,
-    digit
+    digit,
+    roseType,
+    label,
+    level,
+    limitShowNum
   } = args
 
-  let series = {
+  let series = []
+  let levelTemp = {}
+  let rowsTemp = []
+  if (level) {
+    level.forEach((levelItems, index) => {
+      levelItems.forEach(item => { levelTemp[item] = index })
+    })
+    rows.forEach(row => {
+      const itemLevel = levelTemp[row[dimension]]
+      if (itemLevel !== undefined) {
+        if (rowsTemp[itemLevel]) {
+          rowsTemp[itemLevel].push(row)
+        } else {
+          rowsTemp[itemLevel] = [row]
+        }
+      }
+    })
+  } else {
+    rowsTemp.push(rows)
+  }
+  let seriesBase = {
     type: 'pie',
-    radius,
     selectedMode,
     hoverAnimation,
-    data: [],
+    roseType,
     center: ['50%', offsetY]
   }
-  if (percentShow) {
-    series.label = {
-      normal: {
-        show: true,
-        formatter (item) {
-          let tpl = []
-          tpl.push(`${item.name}:`)
-          tpl.push(getFormated(item.value, dataType, digit))
-          tpl.push(`(${item.percent}%)`)
-          return tpl.join(' ')
+  let rowsTempLength = rowsTemp.length
+  rowsTemp.forEach((dataRows, index) => {
+    let seriesItem = Object.assign({ data: [] }, seriesBase)
+    const centerWidth = radius / rowsTempLength
+    if (!index) {
+      seriesItem.radius = centerWidth
+    } else {
+      const outerWidth = centerWidth + radius / (2 * rowsTempLength) * (2 * index - 1)
+      const innerWidth = outerWidth + radius / (2 * rowsTempLength)
+      seriesItem.radius = [outerWidth, innerWidth]
+    }
+    if (rowsTempLength > 1 && index === 0) {
+      seriesItem.label = {
+        normal: { position: 'inner' }
+      }
+    }
+    if (label) seriesItem.label = label
+    if (percentShow) {
+      seriesItem.label = {
+        normal: {
+          show: true,
+          position: rowsTempLength > 1 && index === 0 ? 'inner' : 'outside',
+          formatter (item) {
+            let tpl = []
+            tpl.push(`${item.name}:`)
+            tpl.push(getFormated(item.value, dataType, digit))
+            tpl.push(`(${item.percent}%)`)
+            return tpl.join(' ')
+          }
         }
       }
     }
+    seriesItem.data = dataRows.map(row => ({
+      name: row[dimension],
+      value: row[metrics]
+    }))
+    series.push(seriesItem)
+  })
+  if (limitShowNum) {
+    const firstData = series[0].data
+    const remainArr = firstData.slice(limitShowNum, firstData.length)
+    let sum = 0
+    remainArr.forEach(item => { sum += item.value })
+    series[0].data = firstData.slice(0, limitShowNum)
+    series[0].data.push({ name: '其他', value: sum })
   }
-  series.data = rows.map(row => ({
-    name: row[dimension],
-    value: row[metrics]
-  }))
-
   return series
 }
 
 function getPieLegend (args) {
-  const { rows, dimension, legendLimit } = args
-  let legend = rows.map(row => row[dimension])
+  const { rows, dimension, legendLimit, level, limitShowNum } = args
+  let legend = []
+  const levelTemp = []
+  if (level) {
+    level.forEach(levelItem => {
+      levelItem.forEach(item => {
+        levelTemp.push(item)
+      })
+    })
+    legend = levelTemp
+  } else if (limitShowNum) {
+    for (let i = 0; i < limitShowNum; i++) {
+      legend.push(rows[i][dimension])
+    }
+    legend.push('其他')
+  } else {
+    legend = rows.map(row => row[dimension])
+  }
   return legend.length
     ? { data: legend, show: legend.length < legendLimit }
     : false
 }
 
-function getPieTooltip (dataType, digit) {
+function getPieTooltip (args) {
+  const {
+    dataType,
+    rows,
+    limitShowNum,
+    digit,
+    metrics,
+    dimension
+  } = args
+  let sum = 0
+  const remainArr = rows.map(row => {
+    sum += row[metrics]
+    return {
+      name: row[dimension],
+      value: row[metrics]
+    }
+  })
   return {
     formatter (item) {
       let tpl = []
       tpl.push(itemPoint(item.color))
-      tpl.push(`${item.name}:`)
-      tpl.push(getFormated(item.value, dataType, digit))
-      tpl.push(`(${item.percent}%)`)
+      if (limitShowNum && item.name === '其他') {
+        tpl.push('其他:')
+        remainArr.forEach(({ name, value }) => {
+          const percent = getFormated((value / sum), 'percent')
+          tpl.push(`<br>${name}:`)
+          tpl.push(getFormated(value, dataType, digit))
+          tpl.push(`(${percent})`)
+        })
+      } else {
+        tpl.push(`${item.name}:`)
+        tpl.push(getFormated(item.value, dataType, digit))
+        tpl.push(`(${item.percent}%)`)
+      }
       return tpl.join(' ')
     }
   }
@@ -77,12 +170,20 @@ export const pie = (columns, rows, settings, extra, isRing) => {
     percentShow,
     dimension = columns[0],
     metrics = columns[1],
-    radius = isRing ? ringRadius : pieRadius,
+    roseType = false,
+    radius = isRing
+      ? roseType
+        ? roseRingRadius
+        : ringRadius
+      : pieRadius,
     offsetY = pieOffsetY,
     legendLimit = 30,
     selectedMode = false,
     hoverAnimation = true,
-    digit = 2
+    digit = 2,
+    label = false,
+    level = false,
+    limitShowNum = 0
   } = settings
   const { tooltipVisible, legendVisible } = extra
   const seriesParams = {
@@ -95,11 +196,29 @@ export const pie = (columns, rows, settings, extra, isRing) => {
     offsetY,
     selectedMode,
     hoverAnimation,
-    digit
+    digit,
+    roseType,
+    label,
+    level,
+    limitShowNum
   }
   const series = getPieSeries(seriesParams)
-  const legend = legendVisible && getPieLegend({ rows, dimension, legendLimit })
-  const tooltip = tooltipVisible && getPieTooltip(dataType, digit)
+  const legendParams = {
+    rows,
+    dimension,
+    legendLimit,
+    level,
+    limitShowNum
+  }
+  const legend = legendVisible && getPieLegend(legendParams)
+  const tooltip = tooltipVisible && getPieTooltip({
+    dataType,
+    rows,
+    limitShowNum,
+    digit,
+    metrics,
+    dimension
+  })
   const options = { series, legend, tooltip }
   return options
 }
