c0ae78964b31a80d7765fa39e8e2b28aee99af27
xiguaxigua <1642965215@qq.com>
2018-06-14 20:33:13 +0800

    feat(core): 增加 echarts 响应 width，height 变化

--- examples/test/resize.vue
+++ examples/test/resize.vue
@@ -2,9 +2,13 @@
   <!-- 测试属性
     resizeable: { type: Boolean, default: true },
     resizeDelay: { type: Number, default: 200 },
+    width: { type: String },
+    height: { type: String },
   -->
   <div>
     <ve-line :data="chartData"></ve-line>
+    <button @click="change">change width height</button>
+    <ve-line :data="chartData" :width="chartWidth" :height="chartHeight"></ve-line>
     <ve-line :data="chartData" :resizeable="false"></ve-line>
     <ve-line :data="chartData" :resize-delay="1000"></ve-line>
   </div>
@@ -16,7 +20,15 @@ import { LINE_DATA } from './data'
 export default {
   data () {
     return {
-      chartData: LINE_DATA
+      chartData: LINE_DATA,
+      chartWidth: '400px',
+      chartHeight: '400px'
+    }
+  },
+  methods: {
+    change () {
+      this.chartWidth = this.chartWidth === '400px' ? '300px' : '400px'
+      this.chartHeight = this.chartHeight === '400px' ? '300px' : '400px'
     }
   },
   components: { VeLine }
